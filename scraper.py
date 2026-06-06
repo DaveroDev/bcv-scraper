47ba469d1feb79a8ee1fa8c55c22cbf4323d4af0
Derek Alfonzo <deralex1000@gmail.com>
2026-06-06 01:48:05 -0400

    Fix rate limit exception handler function name

--- scraper.py
+++ scraper.py
@@ -14,7 +14,7 @@ from slowapi.errors import RateLimitExceeded
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
 app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_responder)
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
